debd6513c903450584c036e09e47e2e747056e9b
challaramakrishnareddy52 <challaramakrishnareddy52@gmail.com>
2022-11-17 01:35:21 +0530

    Prime Number

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace PrimeNumber
+{
+    class Program
+    {
+        static bool isPrime(int n)
+        {
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i < Math.Sqrt(n); i++)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+        static void Main()
+        {
+            if (isPrime(11))
+                Console.Write("it is a prime number:");
+
+            else
+                Console.Write("it is not a prime number:");
+        }
+    }
+}
